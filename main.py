5eeb78bc5ae4e473cec8bf01e50c7749c5546ab3
sivaprasad2848 <sivaprasad@cybersquare.org>
2026-09-15 13:21:58 +0530

    Menu Option Added

--- main.py
+++ main.py
@@ -2,10 +2,15 @@
 y=0
 expences=[]
 while(y==0):
-    title=input("Enter the Expence Title")
-    amount=input("Enter the Expence Amount")
-    dt=input("Enter the Expence Date")
-    expences.append((title,amount,dt))
-    #print(title+" "+amount+" "+dt)
-    print(expences)
+    print("1->For Insert")
+    print("2->For Display")
+    opt=int(input("Enter the option"))
+    if opt==1:
+        title=input("Enter the Expence Title")
+        amount=input("Enter the Expence Amount")
+        dt=input("Enter the Expence Date")
+        expences.append((title,amount,dt))
+    if opt==2:
+        #print(title+" "+amount+" "+dt)
+        print(expences)
     y=int(input("Do You Want to Continue? 0 for yes"))
