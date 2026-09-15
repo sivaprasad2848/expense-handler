5b25943a6e043b2b1837d4590675e12c72c104f8
sivaprasad2848 <sivaprasad@cybersquare.org>
2026-09-15 13:10:15 +0530

    List/Tuple, Data History Saved

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #expence manager
-title=input("Enter the Expence Title")
-amount=input("Enter the Expence Amount")
-dt=input("Enter the Expence Date")
-print(title+" "+amount+" "+dt)
+y=0
+expences=[]
+while(y==0):
+    title=input("Enter the Expence Title")
+    amount=input("Enter the Expence Amount")
+    dt=input("Enter the Expence Date")
+    expences.append((title,amount,dt))
+    #print(title+" "+amount+" "+dt)
+    print(expences)
+    y=int(input("Do You Want to Continue? 0 for yes"))
