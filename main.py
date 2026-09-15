b307c727662d0ab333753850af78f91ab201cea1
sivaprasad2848 <sivaprasad@cybersquare.org>
2026-09-15 13:30:01 +0530

    delete,update feature added

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ expences=[]
 while(y==0):
     print("1->For Insert")
     print("2->For Display")
+    print("3->For Delete")
+    print("4->For Update")
     opt=int(input("Enter the option"))
     if opt==1:
         title=input("Enter the Expence Title")
@@ -13,4 +15,13 @@ while(y==0):
     if opt==2:
         #print(title+" "+amount+" "+dt)
         print(expences)
+    if opt==3:
+        i=int(input("Enter the index you want to delete"))
+        del expences[i]
+    if opt==4:
+        i=int(input("Enter the index you want to update"))
+        title=input("Enter the Expence Title")
+        amount=input("Enter the Expence Amount")
+        dt=input("Enter the Expence Date")
+        expences[i]=(title,amount,dt)
     y=int(input("Do You Want to Continue? 0 for yes"))
